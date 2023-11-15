723670ae2b3d7259b886d90bf6b92d01cd48d751
Alin Grigorescu <alin.grig87@gmail.com>
2023-11-15 23:49:40 +0200

    feat: forecast cards, minor fixes

--- api/wheater.js
+++ api/wheater.js
@@ -8,17 +8,7 @@ export const getCurrentWeatherByCity = async (city) => {
 	return weather;
 };
 
-const forecastUrl =
-	'https://api.openweathermap.org/data/2.5/forecast?appid=69518b1f8f16c35f8705550dc4161056&units=metric&q=';
-
-export const getForecastByCity = async (city) => {
-	const response = await fetch(`${forecastUrl}${city}`);
-	const weather = await response.json();
-
-	return weather;
-};
-
-export const getIconForCurrentWeather = async (weather) => {
+export const getIconForCurrentWeather = (weather) => {
 	const iconCode = weather.weather[0].icon;
 	const iconUrl = `https://openweathermap.org/img/wn/${iconCode}.png`;
 
